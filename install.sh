03bb4efc14c294b4c5e825f7c6fdb73175f02f3f
Shohei Fujii <fujii.shohei@gmail.com>
2025-12-12 20:11:54 +0900

    install plugins manually..

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ make install_tpm
 make install_systemd_sshagent
 
 git clone https://github.com/ompugao/vim-bundle ~/.config/nvim
-$HOME/.local/share/mise/shims/nvim -c PlugInstall -c qall
+#$HOME/.local/share/mise/shims/nvim -c PlugInstall -c qall  #this does not work since neovim hangs claiming that no plugins installed
