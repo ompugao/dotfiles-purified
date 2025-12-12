1d902b3e970994fcc24b259c4e6ec1398c7ba632
Shohei Fujii <fujii.shohei@gmail.com>
2025-12-12 19:51:52 +0900

    install nvim config

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ make install_tools
 make install_skkjisho
 make install_tpm
 make install_systemd_sshagent
+
+git clone https://github.com/ompugao/vim-bundle ~/.config/nvim
+nvim -c PlugInstall -c qall
